8bc244987b817086479277c2e0b0032cf1dab8a8
Sippora04 <sippora.toppo95@gmail.com>
2020-11-06 02:23:22 +0530

    Added: searching node in Binary Tree

--- main/java/com/problem14/MyBinaryTree.java
+++ main/java/com/problem14/MyBinaryTree.java
@@ -13,9 +13,9 @@ public class MyBinaryTree<K extends Comparable<K>> {
 		int compResult = key.compareTo(current.key);
 		if (compResult == 0)
 			return current;
-		if (compResult < 0)
+		if (compResult < 0) // if current>key -> left
 			current.left = addRecursively(current.left, key);
-		if (compResult > 0)
+		if (compResult > 0) // current<key ->right
 			current.right = addRecursively(current.right, key);
 		return current;
 	}
@@ -26,5 +26,21 @@ public class MyBinaryTree<K extends Comparable<K>> {
 
 	private int getSizeRecursively(BinaryNode<K> current) {
 		return current == null ? 0 : 1 + this.getSizeRecursively(current.left) + this.getSizeRecursively(current.right);
+	}
+
+	public K search(K key) {
+		return key = this.searchRecursively(root, key);
+	}
+
+	private K searchRecursively(BinaryNode<K> current, K key) {
+		K searchKey = null;
+		if (current == null || current.key == key)
+			return current.key;
+		int compResult = key.compareTo(current.key);
+		if (compResult < 0)
+			searchKey = searchRecursively(current.left, key);
+		if (compResult > 0)
+			searchKey = searchRecursively(current.right, key);
+		return searchKey;
 	}
 }
