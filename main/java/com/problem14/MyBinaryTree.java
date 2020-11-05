e31c63b05bc0e0b6ea64e8a9b127162bf03b279a
Sippora04 <sippora.toppo95@gmail.com>
2020-11-06 02:03:34 +0530

    Added: Welcome Message

---
+++ main/java/com/problem14/MyBinaryTree.java
@@ -0,0 +1,10 @@
+package com.problem14;
+
+public class MyBinaryTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome BST");
+	}
+
+}
